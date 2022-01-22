0cf641542137a51ad3806c703777ebc2ebc1c92b
citelova <98216096+citelova@users.noreply.github.com>
2022-01-22 15:18:42 +0300

    Delete score.js

--- score.js
+++
@@ -1,8 +0,0 @@
-import {objover, objscore} from './init.js'
-export function count ()
-{
-    if (objover.over == 0) {
-        objscore.score -= 1;
-        document.getElementById("score").innerHTML = "Очки: " + objscore.score;
-    }
-}
